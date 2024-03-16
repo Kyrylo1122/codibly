87ffbc3028d615c6ac4da9073491f83ff07e0d45
Kyrylo <bereznov.kyrylo@mail.ru>
2024-03-16 13:05:30 +0100

    save before redux testing

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -1,5 +1,5 @@
 import { useGetProductByIdQuery } from "src/libs/rtk-query";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import ErrorPage from "./ErrorPage";
 
 const style = {
